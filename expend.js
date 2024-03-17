c05a3e1a80b0ac8063bd4e0f6992c504013eecb3
hassanbaloch5234 <87994215+hassanbaloch5234@users.noreply.github.com>
2024-03-17 07:39:44 +0500

    Add files via upload

---
+++ expend.js
@@ -0,0 +1,16 @@
+// function toggleFullScreen(element) {
+//     var fullScreenElement = document.getElementById('fullScreen');
+//     fullScreenElement.classList.add('active');
+//     element.style.width = '100vw';
+//     element.style.height = '100vh';
+// }
+
+// function exitFullScreen() {
+//     var fullScreenElement = document.getElementById('fullScreen');
+//     fullScreenElement.classList.remove('active');
+//     var boxes = document.querySelectorAll('.box');
+//     boxes.forEach(function(box) {
+//         box.style.width = '';
+//         box.style.height = '';
+//     });
+// }
